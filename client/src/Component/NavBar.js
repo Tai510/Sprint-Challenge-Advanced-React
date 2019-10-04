088f7d13992445d533f43cdf8699eccfbd261269
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-10-04 12:41:13 -0700

    styling

--- client/src/Component/NavBar.js
+++ client/src/Component/NavBar.js
@@ -4,7 +4,7 @@ const Navbar = (props) => {
 
   return (
     <nav className="navbar">
-      <h1>Players List</h1>
+      <h1>Players List Dark Mode <i class="fas fa-arrow-right"></i></h1>
       <div className="dark-mode__toggle">
         <div
           onClick={props.toggleMode}
